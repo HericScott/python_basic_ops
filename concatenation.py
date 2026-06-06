9548928167f1ad4902fefe6bcb2c05a3cb69ef4f
scott <hericscott033@gmail.com>
2026-06-06 23:49:10 +0100

    exameple of concatenation

---
+++ concatenation.py
@@ -0,0 +1,6 @@
+#Use concatenation to create your full name from your first and last names. The new string should contain your first name, a space, and your last name. For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
+
+   #solution
+
+name = 'heric' + 'scott'
+print(name)
